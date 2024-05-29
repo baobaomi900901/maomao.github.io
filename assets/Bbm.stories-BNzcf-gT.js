6d2a03209b4e4a3189063aef9ac2324a44e092d1
GitHubActions <GitHubActions@github.com>
2024-05-29 15:19:16 +0000

    Deploying to gh-pages from @ baobaomi900901/maomao.github.io@83ef0a23942f727bdf098fd038d46d28d2e66014 🚀

---
+++ assets/Bbm.stories-BNzcf-gT.js
@@ -0,0 +1,5 @@
+import{d as m,o as n,c as l,r as c,b as d,t as b}from"./vue.esm-bundler-7C2mjde0.js";const p={id:"bbm",class:"bbm text-red-500 h-4 w-4"},r=m({__name:"Bbm",props:{label:{default:"bbm1"}},setup(u){return(a,i)=>(n(),l("div",p,[c(a.$slots,"default",{},()=>[d(b(a.label),1)])]))}});r.__docgenInfo={exportName:"default",displayName:"Bbm",description:"",tags:{},props:[{name:"label",required:!0,type:{name:"string"},defaultValue:{func:!1,value:'"bbm1"'}}],slots:[{name:"default"}],sourceFiles:["/home/runner/work/maomao.github.io/maomao.github.io/src/stories/Bbm/Bbm.vue"]};const _={title:"Example/Bbm",component:r,tags:["autodocs"]},e={args:{label:"Bbm"}};var t,s,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Bbm'
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const g=["test"];export{g as __namedExportsOrder,_ as default,e as test};
